b963c7d919d438074ac5f782c6f750341cfd95ee
thatoneguy087 <adeptpenguin08@gmail.com>
2023-03-19 11:33:04 -0600

    Create containers for calculator elements
    Containers created in the HTML markup for the various parts of the
    calculator. Styling will be much simpler later on.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 'use strict';
+//create basic html markup for the calculator, including all the buttons and the screen
+  //buttons include : clear, backspace, evaluate, +/- changer, add, subtract, multiply, divide, decimal point, number 0-9. Separate items into their own containers.
+//create and test function for add, subtract, multiply, and divide
+//create function operate that will take in one of our operator functions as well as two number and evaluate the expression.
